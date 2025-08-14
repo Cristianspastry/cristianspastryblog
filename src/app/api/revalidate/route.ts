2a6e29539295c3b67252da5be43f88c93f17b53d
QualityChefAgencyReviews <qualitychefagency@gmail.com>
2025-08-14 11:47:23 +0200

    correzione webhook 2

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,4 +1,4 @@
-import { revalidateTag, revalidatePath } from 'next/cache'
+import { revalidatePath } from 'next/cache'
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function POST(request: NextRequest) {
